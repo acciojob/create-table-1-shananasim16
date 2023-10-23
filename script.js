62dbf8512237c77a7bd327c6fce57696c000bc9c
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-23 11:46:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+//function insert_Row() {
+    //Write your code here 
+//}
+
 function insert_Row() {
-    //Write your code here
-  
-  
+    // Get a reference to the table by its ID
+    var table = document.getElementById("sampleTable");
+
+    // Create a new row
+    var newRow = table.insertRow(0); // 0 means insert at the top
+
+    // Create two cells in the new row
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+
+    // Set the values of the cells
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
 }
